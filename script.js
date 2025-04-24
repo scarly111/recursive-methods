26d5df595ba6b93baf5a3a296dd347a9500baa37
scarly111 <yigitcagri48@gmail.com>
2025-04-24 22:10:09 +0300

    recursive fibonacci function

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 // iteration
-function fibs(num){
-    if(num <= 0) return []
-    if(num === 1) return [0]
+function fibs(num) {
+    if (num <= 0) return []
+    if (num === 1) return [0]
 
     const fibsArr = [0, 1]
-    while(fibsArr.length < num){
+    while (fibsArr.length < num) {
         const nextFib = fibsArr[fibsArr.length - 1] + fibsArr[fibsArr.length - 2]
         fibsArr.push(nextFib)
     }
     return fibsArr
 }
 
-console.log(fibs(8))
+// Recursive fibonacci function
+function fibsRec(num) {
+
+    if (num === 0) return 0
+    if (num === 1) return num
+
+    return fibsRec(num - 1) + fibsRec(num - 2)
+}
+
+console.log(fibsRec(3))
